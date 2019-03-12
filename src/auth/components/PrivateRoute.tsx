8cd90590da95dd0ddf1ef5e43ec9b7b195b350cb
Elias Malik <elias0789@gmail.com>
2019-03-12 15:57:07 +0000

    ✨ setup routing

---
+++ src/auth/components/PrivateRoute.tsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Route, RouteProps, Redirect } from 'react-router-dom';
+import { FullScreenBeatLoader } from '../../shared/components/Loaders';
+import { colors } from '../../styles/style_guide';
+import { Roles } from '../../api';
+
+
+/**
+ * Types
+ */
+enum AuthStatus {
+  PENDING = 'PENDING',
+  SUCCESS = 'SUCCESS',
+  FAILURE = 'FAILURE',
+}
+
+interface PrivateRouteProps extends RouteProps {}
+
+interface PrivateRouteState {
+  authStatus: AuthStatus;
+}
+
+
+export default class PrivateRoute extends Component<PrivateRouteProps, PrivateRouteState> {
+
+  static propTypes = {
+    component: PropTypes.oneOfType([PropTypes.func, PropTypes.element]).isRequired,
+  };
+
+  state = {
+    authStatus: AuthStatus.PENDING,
+  };
+
+  componentDidMount () {
+    Roles.get()
+      .then(() => this.setState({ authStatus: AuthStatus.SUCCESS }))
+      .catch(() => this.setState({ authStatus: AuthStatus.FAILURE }));
+  }
+
+  render () {
+    const { component: Comp, ...rest } = this.props;
+
+    switch (this.state.authStatus) {
+      case AuthStatus.PENDING:
+        return (
+          <FullScreenBeatLoader color={colors.highlight_primary} />
+        );
+
+      case AuthStatus.SUCCESS:
+        return (
+          <Route {...rest} component={Comp} />
+        );
+
+      case AuthStatus.FAILURE:
+      default:
+        return (
+          <Redirect to={{ pathname: '/login' }} />
+        );
+    }
+
+  }
+}
